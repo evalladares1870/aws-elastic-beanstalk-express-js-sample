260319d29cf8caf5f06375783984a68795d4617c
evalladares1870 <evalladares@grupomutual.fi.cr>
2022-04-22 16:29:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Bienvenido a Primer Depliegue EVL!'));
+app.get('/', (req, res) => res.send('Hola Bienvenido a Primer Despliegue EVL!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
